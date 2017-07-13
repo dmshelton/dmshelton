1a2993bdd9539305d2275fa2113a7f6f468d6d17
Darren Shelton <dshelton@prometheus.local>
2017-07-13 17:51:24 -0500

    The proverbial Hello World!

---
+++ learningPython/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3 
+
+#BSD 3-Clause License
+
+#Copyright (c) 2017, Darren Shelton
+#All rights reserved.
+
+import sys
+
+def main():
+	print ('Hello Shithead!')
+
+if __name__ == '__main__':
+	main()
