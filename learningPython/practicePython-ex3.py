16e4012401cd807938b3408e906e4ac5ac0609ed
Darren Shelton <dmshelton@gmail.com>
2017-07-16 14:15:24 -0500

    PracticePython.org Exercise 3. Option 1 to the exercise.

--- learningPython/practicePython-ex3.py
+++ learningPython/practicePython-ex3.py
@@ -7,9 +7,11 @@
 def main():
 
     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = []
     for x in a[:]:
         if x < 5:
-            print(x)
+            b.append(x)
+    print(b)
 
 if __name__ == '__main__':
     main()
