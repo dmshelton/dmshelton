b65b7b8255112f5f42c30c6b207eb72937d8318a
Darren Shelton <dmshelton@gmail.com>
2017-07-15 22:26:12 -0500

    PracticePython.org Exercise 1.

--- learningPython/practicePython-ex1.py
+++ learningPython/practicePython-ex1.py
@@ -8,24 +8,30 @@
 # I started teaching myself python utilizing the exercises on http://www.practicepython.org/exercises.
 # This is my solution to Exercise 1.
 
-from datetime import date
 
-thisyear = date.today().year
+def main():
 
-print("Please provide your name and age you have turned in " + str(thisyear) + ".\n")
-name = input("name: ")
-age = input("age: ")
+    from datetime import date
+    thisyear = date.today().year
 
-print("Please enter another number. \n")
-secondnumber = input("number: ")
+    print("Please provide your name and age you have turned in " + str(thisyear) + ".\n")
+    name = input("name: ")
+    age = input("age: ")
 
-year100 = thisyear + (100 - int(age))
+    print("Please enter another number. \n")
+    secondnumber = input("number: ")
 
-i = 0
-while i < int(secondnumber):
-    if int(age) >= 100:
-        print("Congrats, you are already 100 or older.\n")
-        i = i + 1
-    else:
-        print("Hello " + name + ", you will be 100 years old in: " + str(year100) + "\n")
-        i = i + 1
+    year100 = thisyear + (100 - int(age))
+
+    i = 0
+    while i < int(secondnumber):
+        if int(age) >= 100:
+            print("Congrats, you are already 100 or older.\n")
+            i = i + 1
+        else:
+            print("Hello " + name + ", you will be 100 years old in: " + str(year100) + "\n")
+            i = i + 1
+
+
+if __name__ == '__main__':
+    main()
