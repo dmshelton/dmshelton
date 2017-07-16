09a5559a4a75913bd70a5244c7689eca7c6982d2
Darren Shelton <dmshelton@gmail.com>
2017-07-15 21:45:59 -0500

    PracticePython.org Exercise 1.

---
+++ learningPython/practicePython-ex1.py
@@ -0,0 +1,31 @@
+#
+# BSD 3-Clause License
+
+# Copyright (c) 2017, Darren Shelton
+# All rights reserved.
+#
+
+# I started teaching myself python utilizing the exercises on http://www.practicepython.org/exercises.
+# This is my solution to Exercise 1.
+
+from datetime import date
+
+thisyear = date.today().year
+
+print("Please provide your name and age you have turned in " + str(thisyear) + ".\n")
+name = input("name: ")
+age = input("age: ")
+
+print("Please enter another number. \n")
+secondnumber = input("number: ")
+
+year100 = thisyear + (100 - int(age))
+
+i = 0
+while i < int(secondnumber):
+    if int(age) >= 100:
+        print("Congrats, you are already 100 or older.\n")
+        i = i + 1
+    else:
+        print("Hello " + name + ", you will be 100 years old in: " + str(year100) + "\n")
+        i = i + 1
