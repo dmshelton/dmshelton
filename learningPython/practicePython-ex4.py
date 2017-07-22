c35a5f3b72207184834d7fa9bb5c4eaf770b1d4d
Darren Shelton <dmshelton@gmail.com>
2017-07-22 16:20:53 -0500

    PracticePython.org Exercise 4.

--- learningPython/practicePython-ex4.py
+++ learningPython/practicePython-ex4.py
@@ -9,7 +9,6 @@ def main():
     print("Provide a number and I will tell you its divisors")
     number = int(input("what is your number? "))
     check = range(2, number+1)
-
     print("The divisors are: ")
     for i in check:
         result = number % i
